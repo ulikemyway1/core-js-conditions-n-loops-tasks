d940100e84724a55ff9ab3983d30eff107f1177e
Alex <calium_by@mail.ru>
2024-01-07 16:04:28 +0300

    feat: solve some

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,17 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let temp;
+  if (a >= b) {
+    temp = a;
+  } else {
+    temp = b;
+  }
+  if (temp >= c) {
+    return temp;
+  }
+  return c;
 }
 
 /**
